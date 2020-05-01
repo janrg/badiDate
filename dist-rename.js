9f54a0ecc263b3d064cd4eec5fa602c3d4bd229d
janrg <janrg@users.noreply.github.com>
2020-05-01 19:50:50 +0200

    Have a separate file to point "module" to, since some packages still can't deal with .mjs files

--- dist-rename.js
+++ dist-rename.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 // fancy-rollup can't deal with filesnames that only differ by extension so we need to rename afterwards
 for (const filename of fs.readdirSync('dist/')) {
     if (filename.includes('.m.js')) {
-        fs.rename(`dist/${filename}`, `dist/${filename.replace('.m.js', '.mjs')}`, err => {
-            if (err) throw err;
-        });
+        fs.renameSync(`dist/${filename}`, `dist/${filename.replace('.m.js', '.mjs')}`);
     }
 }
+
+// Some packages still can't deal with mjs file extensions
+fs.copyFileSync('dist/localBadiDate-msm-locales.mjs', 'dist/localBadiDate-msm-locales.m.js');
